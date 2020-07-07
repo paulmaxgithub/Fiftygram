0a939663f9b8c4eec3eb1a8b3b7458863b9488f8
PAULMAX <ipaulmax@icloud.com>
2020-07-07 22:22:37 +0300

    What To Do:
    • More Filters
    • Saving Photos
    
    1. In Info.plist file created a new row - "Privacy - Photo Library Additions Usage Description" with value "Fiftygram allows you to save photos to your library.";
    2. Added a new button to the storyboard that will save the photo when pressed.;
    3. Inside of that saveImage() method, implemented saving the photo (UIImageWriteToSavedPhotosAlbum);

--- Fiftygram/ViewController.swift
+++ Fiftygram/ViewController.swift
@@ -1,42 +1,69 @@
 import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    @IBOutlet var imageView: UIImageView!
-
+    
     let context = CIContext()
     var original: UIImage!
+    
+    @IBOutlet var imageView: UIImageView!
 
+    //MARK: - Sepia Button Setting
     @IBAction func applySepia() {
         if original == nil {
             return
         }
-
         let filter = CIFilter(name: "CISepiaTone")
         filter?.setValue(CIImage(image: original), forKey: kCIInputImageKey)
         filter?.setValue(1.0, forKey: kCIInputIntensityKey)
         display(filter: filter!)
     }
 
+    //MARK: - Noir Button Setting
     @IBAction func applyNoir() {
         if original == nil {
             return
         }
-
         let filter = CIFilter(name: "CIPhotoEffectNoir")
         filter?.setValue(CIImage(image: original), forKey: kCIInputImageKey)
         display(filter: filter!)
     }
 
+    //MARK: - Vintage Button Setting
     @IBAction func applyVintage() {
         if original == nil {
             return
         }
-
         let filter = CIFilter(name: "CIPhotoEffectProcess")
         filter?.setValue(CIImage(image: original), forKey: kCIInputImageKey)
         display(filter: filter!)
     }
+    
+    //MARK: -  Color Posterize Button Setting
+    @IBAction func applyPosterize() {
+        if original == nil {
+            return
+        }
+        let filter = CIFilter(name: "CIColorPosterize")
+        filter?.setValue(CIImage(image: original), forKey: kCIInputImageKey)
+        display(filter: filter!)
+    }
+    
+    func display(filter: CIFilter) {
+        let output = filter.outputImage!
+        imageView.image = UIImage(cgImage: self.context.createCGImage(output, from: output.extent)!)
+    }
+    
+    //MARK: - Save Image Button Setting
+    @IBAction func saveImage(_ sender: Any) {
+        guard let image = imageView.image else { return }
+        UIImageWriteToSavedPhotosAlbum(image,
+                                       self,
+                                       #selector(image(_:didFinishSavingWithError:contextInfo:)),
+                                       nil)
+    }
+    
 
+    //MARK: - Bar Button Item Setting
     @IBAction func choosePhoto(_ sender: UIBarButtonItem) {
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
             let picker = UIImagePickerController()
@@ -44,11 +71,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             picker.sourceType = .photoLibrary
             self.navigationController?.present(picker, animated: true, completion: nil)
         }
-    }
-
-    func display(filter: CIFilter) {
-        let output = filter.outputImage!
-        imageView.image = UIImage(cgImage: self.context.createCGImage(output, from: output.extent)!)
     }
 
     func imagePickerController(
@@ -60,5 +82,22 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             imageView.image = image
             original = image
         }
+    }
+    
+    @objc func image(_ image: UIImage,
+                     didFinishSavingWithError error: Error?,
+                     contextInfo: UnsafeRawPointer) {
+        
+        if let error = error {
+            // we got back an error!
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+        
     }
 }
